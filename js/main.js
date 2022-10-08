8a5ff55d7e5c19279d108b9fdd967797ea04ff29
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-09 02:31:06 +0600

    home page added

--- js/main.js
+++ js/main.js
@@ -270,3 +270,48 @@ $('.btn-pay').click(function () {
     $('.btn-pay').removeClass('active')
     $(this).addClass('active')
 })
+
+
+$('.center-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+})
+$('.single-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
